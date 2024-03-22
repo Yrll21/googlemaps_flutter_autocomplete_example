6bd87071a4d969f7093d2e07ef8e142419aa2f6b
Yrll <ivanyrlllabadia@gmail.com>
2024-03-22 07:51:34 +0000

    Update Android configuration and dependencies

--- README.md
+++ README.md
@@ -1,16 +1,67 @@
 # autocomplete_example
 
-A new Flutter project.
+This project is an example project that demonstrates the usage of `google_maps_flutter` to load a map and `googlemaps_flutter_webservices` package to show autocomplete predictions with a search box. It also includes functionality to move the user to the selected place when selecting a suggestion. Additionally, the project implements session tokens using the `UUID` library.
 
-## Getting Started
+## Getting Started with Google Maps API
 
-This project is a starting point for a Flutter application.
+To use this project, you'll need a Google Maps API key.
 
-A few resources to get you started if this is your first Flutter project:
+Google provides detailed documentation on how to get started with the Google Maps API, including how to create a project in the Google Cloud Console, enable the necessary APIs, and create an API key.
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+Please follow the instructions in the [Google Maps Platform Documentation](https://developers.google.com/maps/gmp-get-started) to get your API key.
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Once you have your API key, open the project in your code editor. Create a new file in the `lib` directory and name it `api_keys.dart`. In `api_keys.dart`, define a constant for your API key:
+
+    ```dart
+    const String googleMapsApiKey = 'YOUR_API_KEY';
+    ```
+
+Replace `'YOUR_API_KEY'` with your actual API key.
+
+Remember to add `api_keys.dart` to your `.gitignore` file to prevent your API key from being committed to your version control system. This is important for keeping your API key secure.
+
+Once you have your Google Maps API key, you'll need to add it for other platform implementations also. The process is slightly different for iOS and Android.s
+
+### Android
+
+1. Open the `android/app/src/main/AndroidManifest.xml` file.
+2. Look for the following meta-data tag:
+
+   ```xml
+   <meta-data
+       android:name="com.google.android.geo.API_KEY"
+       android:value="API_KEY"/>
+   ```
+
+3. Replace `API_KEY` with your actual API key.
+
+### iOS
+
+1. Open the `ios/Runner/AppDelegate.swift` file (or `AppDelegate.m` if your project is using Objective-C).
+2. Add the following import at the top of the file:
+
+   ```swift
+   import GoogleMaps
+   ```
+
+   If your project is using Objective-C, use this import instead:
+
+   ```objective-c
+   #import <GoogleMaps/GoogleMaps.h>
+   ```
+
+3. Add the following line to the `application:didFinishLaunchingWithOptions:` method:
+
+   ```swift
+   GMSServices.provideAPIKey("API_KEY")
+   ```
+
+   If your project is using Objective-C, use this line instead:
+
+   ```objective-c
+   [GMSServices provideAPIKey:@"API_KEY"];
+   ```
+
+4. Replace `API_KEY` with your actual API key.
+
+Remember to keep your API key secure. Do not commit your API key to your version control system.
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 // ignore_for_file: library_private_types_in_public_api
 
-import 'dart:convert';
-
+import 'package:autocomplete_example/api_keys.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-// add http package
-import 'package:http/http.dart' as http;
+import 'package:googlemaps_flutter_webservices/places.dart';
+import 'package:uuid/uuid.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-const apiKey = "AIzaSyCoeQGGbzMjhMf8G3xwJ2KtEMoGndZ6ktQ";
+const apiKey = googleMapsApiKey;
 
 void main() {
   runApp(const MyApp());
@@ -36,11 +36,22 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  // controllers for the map and search bar
   GoogleMapController? _mapController;
-  var sessionToken = DateTime.now().millisecondsSinceEpoch;
   final _searchController = TextEditingController();
-  List _predictions = [];
 
+  // this is where we store the predictions for the ListView.builder
+  final _predictions = [];
+  // create storage for markers
+  final Set<Marker> _markers = {};
+
+  // create a new instance of GoogleMapsPlaces
+  final places = GoogleMapsPlaces(apiKey: apiKey);
+
+  // create a new session token using the Uuid package
+  var sessionToken = const Uuid().v4();
+
+  // build the widget
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,11 +69,12 @@ class _MapScreenState extends State<MapScreen> {
                 target: LatLng(37.7749, -122.4194), // San Francisco coordinates
                 zoom: 12,
               ),
+              markers: _markers,
             ),
           ),
           Positioned(
             // Change background color to white
-            top: 10,
+            top: 0,
             left: 0,
             right: 0,
             child: Column(
@@ -87,9 +99,62 @@ class _MapScreenState extends State<MapScreen> {
                           tileColor:
                               Colors.white, // Change background color to white
                           title: Text(_predictions[index]["description"]),
+                          onTap: () async {
+                            var placeId = _predictions[index]["placeId"];
+                            var res = await places.getDetailsByPlaceId(placeId,
+                                sessionToken: sessionToken);
+                            if (res.isOkay) {
+                              var lat = res.result.geometry?.location.lat;
+                              var lng = res.result.geometry?.location.lng;
+                              _mapController!.animateCamera(
+                                // move the camera and zoom
+                                CameraUpdate.newLatLngZoom(
+                                  LatLng(lat!, lng!),
+                                  15,
+                                ),
+                              );
+
+                              // add a marker to the map
+                              _markers.add(
+                                Marker(
+                                  markerId: MarkerId(placeId),
+                                  position: LatLng(lat, lng),
+                                  infoWindow: InfoWindow(
+                                    title: res.result.name,
+                                    snippet: res.result.formattedAddress,
+                                  ),
+                                ),
+                              );
+                              // clear predictions and replace the search bar text
+                              setState(() {
+                                _predictions.clear();
+                                _searchController.text = res.result.name;
+                                // reset the session token
+                                sessionToken = const Uuid().v4();
+                              });
+                            }
+                          },
                         ),
                       );
-                    })
+                    }),
+                Container(
+                  // fill max width
+                  width: double.infinity,
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    // Create a row with text and image for Google attribution
+                    child: Row(
+                      children: [
+                        const Text(
+                          "Powered by ",
+                        ),
+                        // google attribution icon from asset folder
+                        Image.asset("assets/google_on_white.png"),
+                      ],
+                    ),
+                  ),
+                )
               ],
             ),
           ),
@@ -100,50 +165,34 @@ class _MapScreenState extends State<MapScreen> {
 
   // define handlePress function
   void handleChange(value) async {
-    // make an autocomplete request
-    final response = await http.get(Uri.parse(
-        'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$value&types=establishment&key=$apiKey&sessiontoken=$sessionToken'));
-    // create an autocomplete prediction list below the TextInput
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body)['predictions'] as List<dynamic>;
-      // print the data to the console
-      // print("Data result: $data");
-      // map through the data and store it in the _predictions list
-      final predictions = data
-          .map((prediction) => {
-                "description": prediction['description'],
-                "place_id": prediction['place_id']
-              })
-          .toList();
-      // print the predictions to the console
-      print("Predictions result: $predictions");
-      // set the state of the _predictions list
+    // this is to check if session tokens have been implemented correctly
+    print("Session Token: $sessionToken");
+    var predictions = [];
+    var res = await places.autocomplete(value,
+        sessionToken: sessionToken.toString(), types: ["establishment"]);
+    if (res.isOkay) {
+      for (var p in res.predictions) {
+        predictions.add({"description": p.description, "placeId": p.placeId});
+      }
       setState(() {
-        _predictions = predictions;
+        _predictions.clear();
+        _predictions.addAll(predictions);
       });
     } else {
-      // show an alert dialog if the response is not successful
-      showError();
+      showError(res.errorMessage);
     }
   }
 
-  void showError() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Error'),
-          content: const Text('An error occurred while searching for places'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Close'),
-            ),
-          ],
-        );
-      },
+  showError(error) {
+    // toast the error using Fluttertoast package
+    Fluttertoast.showToast(
+      msg: error,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.red,
+      textColor: Colors.white,
+      fontSize: 16.0,
     );
   }
 }
